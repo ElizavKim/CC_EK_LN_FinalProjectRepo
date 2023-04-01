0b211169bad07ef48253d857b3e3fb6c628a2c54
ElizavKim <elizav.kim@gmail.com>
2023-03-31 20:25:57 -0400

    created JavaFX project

---
+++ CC_EK_LN_FinalProject/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cc_ek_ln_finalproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cc_ek_ln_finalproject to javafx.fxml;
+    exports com.example.cc_ek_ln_finalproject;
+}
