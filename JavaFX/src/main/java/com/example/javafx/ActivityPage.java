4fa6aa29a69e845191c5a2f805724881de646365
lilcircle <cc926@cairn.edu>
2023-04-05 15:15:19 -0400

    Added Activity Options Page

---
+++ JavaFX/src/main/java/com/example/javafx/ActivityPage.java
@@ -0,0 +1,11 @@
+package com.example.javafx;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import java.io.IOException;
+public class ActivityPage {
+}
